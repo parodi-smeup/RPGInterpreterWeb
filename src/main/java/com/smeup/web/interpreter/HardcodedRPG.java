5775fcd477e7ccd5a4c05a65fe414d5df21b13cc
Franco Parodi <franco.parodi@smeup.com>
2019-06-07 11:20:00 +0200

    Fix call java program

--- src/main/java/com/smeup/web/interpreter/HardcodedRPG.java
+++ src/main/java/com/smeup/web/interpreter/HardcodedRPG.java
@@ -2,18 +2,174 @@ package com.smeup.web.interpreter;
 
 public enum HardcodedRPG {
 	
-    HELLOWORLD("﻿     D Msg             S             12\n" + 
-			"     C                   Eval      Msg  = 'Hello World!'\n" + 
+    HELLO_WORLD("﻿     D Msg             S             50\n" + 
+			"     C                   Eval      Msg  = 'Hello World !'\n" + 
 			"     C                   dsply                   Msg\n" + 
 			"     C                   SETON                                          LR"),
     
+    ENTRY_PLIST("     V*=====================================================================\n" + 
+    		"     V* Date      Release Au Description\n" + 
+    		"     V* dd/mm/yy  V5R1    AD Anonymous Developer\n" + 
+    		"     V*=====================================================================\n" + 
+    		"     H/COPY QILEGEN,£INIZH\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     I/COPY QILEGEN,£TABB£1DS\n" + 
+    		"     I/COPY QILEGEN,£PDS\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"      * ENTRY\n" + 
+    		"      * . Function\n" + 
+    		"     D U$FUNZ          S             10\n" + 
+    		"      * . Method\n" + 
+    		"     D U$METO          S             10\n" + 
+    		"      * . String\n" + 
+    		"     D U$SVAR          S         210000\n" + 
+    		"      * . Return Code\n" + 
+    		"     D U$IN35          S              1\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     D DSP             S             50\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     D* M A I N\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     C     *ENTRY        PLIST\n" + 
+    		"     C                   PARM                    U$FUNZ\n" + 
+    		"     C                   PARM                    U$METO\n" + 
+    		"     C                   PARM                    U$SVAR\n" + 
+    		"     C                   PARM                    U$IN35\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='Entry parms received:'                  COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='U$FUNZ='+ U$FUNZ                        COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='U$METO='+ U$METO                        COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='U$SVAR='+ U$SVAR                        COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='U$IN35='+ U$IN35                        COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      * Initial settings\n" + 
+    		"     C                   EXSR      IMP0\n" + 
+    		"\n" + 
+    		"      * Final settings\n" + 
+    		"     C                   EXSR      FIN0\n" + 
+    		"      * End\n" + 
+    		"      *\n" + 
+    		"     C                   SETON                                        LR\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     C/COPY QILEGEN,£INZSR\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"    RD* Initial subroutine (as *INZSR)\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     £INIZI        BEGSR\n" + 
+    		"      *\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"    RD* Initial settings\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     IMP0          BEGSR\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='IMP0 EXECUTED'                          COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"    RD* Final settings\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     FIN0          BEGSR\n" + 
+    		"      *\n" + 
+    		"     C                   EVAL      DSP='FIN0 EXECUTED'                          COSTANTE\n" + 
+    		"     C                   DSPLY                   DSP\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*"),
+    
+    JAVA_CALL("     V*=====================================================================\n" + 
+    		"     V* Date      Release Au Description\n" + 
+    		"     V* dd/mm/yy  V5R1    AD Anonymous Developer\n" + 
+    		"     V*=====================================================================\n" + 
+    		"     H/COPY QILEGEN,£INIZH\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     I/COPY QILEGEN,£TABB£1DS\n" + 
+    		"     I/COPY QILEGEN,£PDS\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"      * Invoke Url\n" + 
+    		"      * . Function\n" + 
+    		"     D §§FUNZ          S             10\n" + 
+    		"      * . Method\n" + 
+    		"     D §§METO          S             10\n" + 
+    		"      * . Url\n" + 
+    		"     D §§SVAR          S           1000\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     D DSP             S             50\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     D* M A I N\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"      * Initial settings\n" + 
+    		"     C                   EXSR      IMP0\n" + 
+    		"      * Call java program\n" + 
+    		"     C                   EXSR      FESE\n" + 
+    		"      * Final settings\n" + 
+    		"     C                   EXSR      FIN0\n" + 
+    		"      * End\n" + 
+    		"     C                   SETON                                        LR\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"     C/COPY QILEGEN,£INZSR\n" + 
+    		"      *---------------------------------------------------------------\n" + 
+    		"    RD* Initial subroutine (as *INZSR)\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     £INIZI        BEGSR\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"    RD* Initial settings\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     IMP0          BEGSR\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"    RD* Final settings\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     FIN0          BEGSR\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"    RD* Ese\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     FESE          BEGSR\n" + 
+    		"      *\n" + 
+    		"      * Invoke url\n" + 
+    		"     C                   EVAL      §§FUNZ='URL'                                 COSTANTE \n" + 
+    		"     C                   EVAL      §§METO='HTTP'                                COSTANTE\n" + 
+    		"     C                   EVAL      §§SVAR='http://www.mocky.io/v2/'+            COSTANTE\n" + 
+    		"     C                             '5185415ba171ea3a00704eed'                                    \n" + 
+    		"      *\n" + 
+    		"     C                   CALL      'Jd_url'\n" + 
+    		"     C                   PARM                    §§FUNZ\n" + 
+    		"     C                   PARM                    §§METO\n" + 
+    		"     C                   PARM                    §§SVAR\n" + 
+    	    "     C                   EVAL      DSP=§§SVAR\n" +
+    	    "     C                   DSPLY                   DSP\n" +
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"    RD* Detach\n" + 
+    		"      *--------------------------------------------------------------*\n" + 
+    		"     C     FCLO          BEGSR\n" + 
+    		"      *\n" + 
+    		"     C                   ENDSR\n" + 
+    		""),
+    
     FIBONACCI("﻿      * Calculates number of Fibonacci in an iterative way\n" + 
 			"     D ppdat           S              8\n" + 
-			"     D NBR             S              8  0\n" + 
-			"     D RESULT          S              8  0 INZ(0)\n" + 
-			"     D COUNT           S              8  0\n" + 
-			"     D A               S              8  0 INZ(0)\n" + 
-			"     D B               S              8  0 INZ(1)\n" + 
+			"     D NBR             S             21  0\n" + 
+			"     D RESULT          S             21  0 INZ(0)\n" + 
+			"     D COUNT           S             21  0\n" + 
+			"     D A               S             21  0 INZ(0)\n" + 
+			"     D B               S             21  0 INZ(1)\n" + 
 			"     C     *entry        plist\n" + 
 			"     C                   parm                    ppdat                          I\n" + 
 			"      *\n" + 
@@ -40,157 +196,7 @@ public enum HardcodedRPG {
 			"     C                   ENDSL\n" + 
 			"     C                   ENDSR\n" + 
 			"      *--------------------------------------------------------------*\n" + 
-			""),
-    
-    JD_001B("     V*=====================================================================\n" + 
-    		"     V* Date      Release Au Description\n" + 
-    		"     V* dd/mm/yy  nn.mm   xx Brief description\n" + 
-    		"     V*=====================================================================\n" + 
-    		"     V* 27/05/19  V5R1    AD Anonymous Developer\n" + 
-    		"     V* 29/05/19  V5R1    AD More verbose with dsply\n" + 
-    		"     V*=====================================================================\n" + 
-    		"     H/COPY QILEGEN,£INIZH\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"     I/COPY QILEGEN,£TABB£1DS\n" + 
-    		"     I/COPY QILEGEN,£PDS\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"      * ENTRY\n" + 
-    		"      * . Function\n" + 
-    		"     D U$FUNZ          S             10\n" + 
-    		"      * . Method\n" + 
-    		"     D U$METO          S             10\n" + 
-    		"      * . String\n" + 
-    		"     D U$SVAR          S         210000\n" + 
-    		"      * . Return Code ('1'=ERROR / blank=OK)\n" + 
-    		"     D U$IN35          S              1\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"      * . Url\n" + 
-    		"     D $$URL           S           1000\n" + 
-    		"     D $X              S              3  0\n" + 
-    		"     D $$SVAR          S                   LIKE(U$SVAR)\n" + 
-    		"     D XXSVAR          S                   LIKE(U$SVAR)\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"      * Invoke Url\n" + 
-    		"      * . Function\n" + 
-    		"     D §§FUNZ          S             10\n" + 
-    		"      * . Method\n" + 
-    		"     D §§METO          S             10\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"     D DSP             S             50\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"     D* M A I N\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"     C     *ENTRY        PLIST\n" + 
-    		"     C                   PARM                    U$FUNZ\n" + 
-    		"     C                   PARM                    U$METO\n" + 
-    		"     C                   PARM                    U$SVAR\n" + 
-    		"     C                   PARM                    U$IN35\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='JD_001B Entry parms:'                   COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='FUNZ='+%TRIM(U$FUNZ)+                   COSTANTE\n" + 
-    		"     C                             ',METO='+%TRIM(U$METO)+\n" + 
-    		"     C                             ',SVAR='+%TRIM(U$SVAR)+\n" + 
-    		"     C                             ',IN35='+%TRIM(U$IN35)\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"      * Initial settings\n" + 
-    		"     C                   EXSR      IMP0\n" + 
-    		"      * Function / Method\n" + 
-    		"1    C                   SELECT\n" + 
-    		"      * Init\n" + 
-    		"1x   C                   WHEN      U$FUNZ='INZ'\n" + 
-    		"     C                   EXSR      FINZ\n" + 
-    		"      * Invoke URL\n" + 
-    		"1x   C                   WHEN      U$FUNZ='ESE'\n" + 
-    		"     C                   EXSR      FESE\n" + 
-    		"      * Detach (empty subroutine in this case)\n" + 
-    		"1x   C                   WHEN      U$FUNZ='CLO'\n" + 
-    		"     C                   EXSR      FCLO\n" + 
-    		"1e   C                   ENDSL\n" + 
-    		"      * Final settings\n" + 
-    		"     C                   EXSR      FIN0\n" + 
-    		"      * End\n" + 
-    		"     C                   EVAL      DSP='END PROGRAM (RT)'                       COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   SETON                                        RT\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"     C/COPY QILEGEN,£INZSR\n" + 
-    		"      *---------------------------------------------------------------\n" + 
-    		"    RD* Initial subroutine (as *INZSR)\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"     C     £INIZI        BEGSR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='£INIZI EXECUTED'                        COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   ENDSR\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"    RD* Initial settings\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"     C     IMP0          BEGSR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='IMP0 EXECUTED'                          COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   ENDSR\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"    RD* Final settings\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"     C     FIN0          BEGSR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='FIN0 EXECUTED'                          COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   ENDSR\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"    RD* Init\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"     C     FINZ          BEGSR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      U$IN35=*BLANKS\n" + 
-    		"     C                   EVAL      $$SVAR=U$SVAR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='FINZ EXECUTED'                          COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   ENDSR\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"    RD* Invoke\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"     C     FESE          BEGSR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      U$IN35=*BLANKS\n" + 
-    		"      * Invoke url\n" + 
-    		"     C                   EVAL      §§FUNZ='URL'\n" + 
-    		"     C                   EVAL      §§METO='HTTP'\n" + 
-    		"     C                   EVAL      XXSVAR=%TRIM($$SVAR)+U$SVAR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='CALLING PGM JD_URL'                     COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   CALL      'JD_URL'\n" + 
-    		"     C                   PARM                    §§FUNZ\n" + 
-    		"     C                   PARM                    §§METO\n" + 
-    		"     C                   PARM                    XXSVAR\n" + 
-    		"      *\n" + 
-    		"     C                   EVAL      DSP='JD_URL CALLED, FESE EXECUTED'           COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   ENDSR\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"    RD* Detach\n" + 
-    		"      *--------------------------------------------------------------*\n" + 
-    		"     C     FCLO          BEGSR\n" + 
-    		"      *\n" + 
-    		"      * This function doesn't do anything and is always successfull\n" + 
-    		"     C                   EVAL      DSP='FCLO EXECUTED'                          COSTANTE\n" + 
-    		"     C                   DSPLY                   DSP\n" + 
-    		"      *\n" + 
-    		"     C                   ENDSR\n" + 
-    		"");
+			"");
  
     private String source;
  
--- src/main/java/com/smeup/web/interpreter/RpgController.java
+++ src/main/java/com/smeup/web/interpreter/RpgController.java
@@ -1,7 +1,6 @@
 package com.smeup.web.interpreter;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
@@ -13,7 +12,6 @@ import javax.faces.component.UIOutput;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.management.RuntimeErrorException;
 
 import com.smeup.rpgparser.CommandLineProgram;
 import com.smeup.rpgparser.RunnerKt;
@@ -25,39 +23,40 @@ public class RpgController implements Serializable {
 
 	/**
 	 * WARNING: On Windows remember to start payara with this JVM option:
-	 *  -Dfile.encoding=UTF-8
+	 * -Dfile.encoding=UTF-8
 	 */
 	private static final long serialVersionUID = 1L;
 	private CommandLineProgram commandLineProgram;
+	private JavaSystemInterface javaSystemInterface;
+	private ByteArrayOutputStream byteArrayOutputStream;
+	private PrintStream printStream;
 
 	@Inject
 	private RpgSource rpgSource;
 
 	private String interpretationOutput;
-	
 	private String rpgPreloaded;
-	private Map<String,Object> rpgPreloadedValues;
-	
+	private Map<String, Object> rpgPreloadedValues;
 	private String elapsedTime;
+
 	@PostConstruct
 	public void initPreloadedContent() {
-		// load all Jd_* programs (a java programm called as an RPG from an interpreted RPG)
-
-		rpgPreloadedValues = new LinkedHashMap<String,Object>();
-		rpgPreloadedValues.put("", "");
-		rpgPreloadedValues.put("Hello world", HardcodedRPG.HELLOWORLD.getSource());
+		rpgPreloadedValues = new LinkedHashMap<String, Object>();
+		rpgPreloadedValues.put("Samples...", "");
+		rpgPreloadedValues.put("Hello world", HardcodedRPG.HELLO_WORLD.getSource());
+		rpgPreloadedValues.put("Entry plist", HardcodedRPG.ENTRY_PLIST.getSource());
 		rpgPreloadedValues.put("Fibonacci", HardcodedRPG.FIBONACCI.getSource());
-		rpgPreloadedValues.put("JD_001B", HardcodedRPG.JD_001B.getSource());
+		rpgPreloadedValues.put("Java call", HardcodedRPG.JAVA_CALL.getSource());
 	}
 
-	public Map<String,Object> getRpgPreloadedValue() {
+	public Map<String, Object> getRpgPreloadedValue() {
 		return rpgPreloadedValues;
 	}
-	
+
 	public String getFavCoffee2() {
 		return rpgPreloaded;
 	}
-	
+
 	public String getRpgContent() {
 		return rpgSource.getContent();
 	}
@@ -65,7 +64,7 @@ public class RpgController implements Serializable {
 	public void setRpgContent(final String content) {
 		rpgSource.setContent(content);
 	}
-	
+
 	public String getRpgParmList() {
 		return rpgSource.getRpgParmList();
 	}
@@ -73,37 +72,36 @@ public class RpgController implements Serializable {
 	public void setRpgParmList(final String parmList) {
 		rpgSource.setRpgParmList(parmList);
 	}
-	
+
 	public void interpretate() {
 		String rpgSource = lineEndingConversion(getRpgContent());
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-
-		JavaSystemInterface javaSystemInterface = new JavaSystemInterface(ps);
-		javaSystemInterface.addJavaInteropPackage("com.smeup.jd");
+		// To handle system.out response
+		byteArrayOutputStream = new ByteArrayOutputStream();
+		printStream = new PrintStream(byteArrayOutputStream);
+		javaSystemInterface = new JavaSystemInterface(printStream);
 		
+		// load Jd_url commandLineProgram (a java programm called as an RPG from an interpreted RPG)
+		javaSystemInterface.addJavaInteropPackage("com.smeup.jd");
 		commandLineProgram = RunnerKt.getProgram(rpgSource, javaSystemInterface);
 		commandLineProgram.setTraceMode(false);
 
 		List<String> parms = new ArrayList<String>();
-		if(null != getRpgParmList() && !"".equals(getRpgParmList())) {
+		if (null != getRpgParmList() && !"".equals(getRpgParmList())) {
 			String[] splitted = getRpgParmList().split("\\|");
 			parms = Arrays.asList(splitted);
 		}
-		String response = executeRunnerKt(parms, baos);
-		baos.reset();
+		String response = callProgram(parms);
 		setInterpretationOutput(response);
 	}
-
 
 	private String lineEndingConversion(String rpgContent) {
 		Scanner scanner = new Scanner(rpgContent);
 		StringBuilder result = new StringBuilder();
 		while (scanner.hasNextLine()) {
-		  String line = scanner.nextLine();
-		  result.append(line);
-		  result.append(System.lineSeparator());
+			String line = scanner.nextLine();
+			result.append(line);
+			result.append(System.lineSeparator());
 		}
 		scanner.close();
 		return result.toString();
@@ -117,12 +115,15 @@ public class RpgController implements Serializable {
 		this.interpretationOutput = interpretationOutput;
 	}
 
-	private String executeRunnerKt(final List<String> parms, ByteArrayOutputStream baos) {
+	private String callProgram(final List<String> parms) {
 		Instant beginOperation = Instant.now();
 		commandLineProgram.singleCall(parms);
 		Instant endOperation = Instant.now();
 		calculateElapsedTime(beginOperation, endOperation);
-		return new String(baos.toByteArray(), StandardCharsets.UTF_8);
+		String response = new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8);
+		byteArrayOutputStream.reset();
+
+		return response;
 	}
 
 	public String getRpgPreloaded() {
@@ -132,18 +133,19 @@ public class RpgController implements Serializable {
 	public void setRpgPreloaded(String rpgPreloaded) {
 		this.rpgPreloaded = rpgPreloaded;
 	}
-	
-	public void valueChangeMethod(AjaxBehaviorEvent abe){
+
+	public void valueChangeMethod(AjaxBehaviorEvent abe) {
 		setRpgContent(programFromGUI(abe));
 	}
 
 	private String programFromGUI(AjaxBehaviorEvent abe) {
-		return (String) ((UIOutput)abe.getSource()).getValue();
+		return (String) ((UIOutput) abe.getSource()).getValue();
 	}
-	
+
 	private void calculateElapsedTime(final Instant beginOperation, final Instant endOperation) {
 		Duration duration = Duration.between(beginOperation, endOperation);
-		setElapsedTime("Started: " + beginOperation.toString() + "<br>Ended: " + endOperation + "<br>Elapsed: " + humanReadableFormat(duration));
+		setElapsedTime("Started: " + beginOperation.toString() + "<br>Ended: " + endOperation + "<br>Elapsed: "
+				+ humanReadableFormat(duration));
 	}
 
 	public void setElapsedTime(String elapsedTime) {
@@ -153,12 +155,9 @@ public class RpgController implements Serializable {
 	public String getElapsedTime() {
 		return elapsedTime;
 	}
-	
+
 	public static String humanReadableFormat(Duration duration) {
-	    return duration.toString()
-	            .substring(2)
-	            .replaceAll("(\\d[HMS])(?!$)", "$1 ")
-	            .toLowerCase();
+		return duration.toString().substring(2).replaceAll("(\\d[HMS])(?!$)", "$1 ").toLowerCase();
 	}
 
 }
